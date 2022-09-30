023b47fea86913e78f49170ba0a3197f8f31ed8b
DarkLord76865 <40371578+darklord76865@users.noreply.github.com>
2022-09-30 21:57:19 +0200

    fix comment

--- Croatian/PN/Python/PN.py
+++ Croatian/PN/Python/PN.py
@@ -21,4 +21,4 @@ for i in range(r1 + 1, r2 + 1):
 vertikalno_zbrojeno *= (s2 - s1 + 1)
 
 print(jedinice_u_polju + vodoravno_zbrojeno + vertikalno_zbrojeno)
-# ovaj način može biti sporiji od 1. - Multiples of 3 or 5 sekunde (ovisno o brzini procesora), isto rješenje u Rustu daje željenu brzinu (pogledajte folder Rust)
+# ovaj način može biti sporiji od 1 sekunde (ovisno o brzini procesora), isto rješenje u Rustu daje željenu brzinu (pogledajte folder Rust)
