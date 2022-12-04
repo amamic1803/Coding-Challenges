aa9f2e45789c90d81e32d2ac3e45dca9eda63c12
DarkLord76865 <40371578+darklord76865@users.noreply.github.com>
2022-12-04 12:10:56 +0100

    Add Advent of Code

---
+++ English/Advent of Code/2022/01/Rust/main.rs
@@ -0,0 +1,17 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let mut elf_calories: Vec<u64> = vec![];
+    let mut curr_calorie: u64 = 0;
+    for line in input.split("\n") {
+        if line != "" {
+            curr_calorie += line.parse::<u64>().unwrap();
+        } else {
+            elf_calories.push(curr_calorie);
+            curr_calorie = 0;
+        }
+    }
+    elf_calories.sort();
+    println!("{}\n{}", elf_calories[elf_calories.len() - 1], elf_calories[(elf_calories.len() - 3)..].iter().sum::<u64>());
+}
