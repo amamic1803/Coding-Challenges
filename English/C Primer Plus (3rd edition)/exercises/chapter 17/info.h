3228540f8031cd671d8ce898a859067e58d1c646
DarkLord76865 <mmima191@gmail.com>
2023-08-03 14:15:50 +0200

    c primer plus - chapter 17 exercise 3

--- English/C Primer Plus (3rd edition)/exercises/chapter 17/info.h
+++ English/C Primer Plus (3rd edition)/exercises/chapter 17/info.h
@@ -2,3 +2,4 @@
 
 void ch17_ex01(void);
 void ch17_ex02(void);
+void ch17_ex03(void);
--- English/C Primer Plus (3rd edition)/main.c
+++ English/C Primer Plus (3rd edition)/main.c
@@ -813,7 +813,7 @@ static void run(int chapter, int exercise, int arg_count, char **arg_values) {
                     break;
                 }
                 case 3: {
-                    //ch17_ex03();
+                    ch17_ex03();
                     break;
                 }
                 case 4: {
