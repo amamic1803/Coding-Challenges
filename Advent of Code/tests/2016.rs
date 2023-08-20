6c9de6a8ab1589a5407916b0aae8fbd6b18ae857
DarkLord76865 <mmima191@gmail.com>
2023-08-20 14:01:46 +0200

    Advent of Code - Year 2016 - Day 02

--- Advent of Code/tests/2016.rs
+++ Advent of Code/tests/2016.rs
@@ -11,3 +11,13 @@ fn year2016_day01_part1() {
 fn year2016_day01_part2() {
     run_test(2016, 1, 2, "150");
 }
+
+#[test]
+fn year2016_day02_part1() {
+    run_test(2016, 2, 1, "61529");
+}
+
+#[test]
+fn year2016_day02_part2() {
+    run_test(2016, 2, 2, "C2C28");
+}
