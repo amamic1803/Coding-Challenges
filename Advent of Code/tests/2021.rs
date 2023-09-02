be3b66fbbe7929f034d35586eb7efff54c991b7e
DarkLord76865 <mmima191@gmail.com>
2023-09-02 17:42:43 +0200

    Advent of Code - Year 2021 - Day 02

--- Advent of Code/tests/2021.rs
+++ Advent of Code/tests/2021.rs
@@ -11,3 +11,13 @@ fn year2021_day01_part1() {
 fn year2021_day01_part2() {
     run_test(2021, 1, 2, "1822");
 }
+
+#[test]
+fn year2021_day02_part1() {
+    run_test(2021, 2, 1, "1459206");
+}
+
+#[test]
+fn year2021_day02_part2() {
+    run_test(2021, 2, 2, "1320534480");
+}
