db135328e13b83e0710bb2807f1e0ecd746c59e5
DarkLord76865 <mmima191@gmail.com>
2024-04-03 00:01:04 +0200

    add AoC-2020-08

--- Advent of Code/src/challenges/year_2020/day_08/mod.rs
+++ Advent of Code/src/challenges/year_2020/day_08/mod.rs
@@ -10,7 +10,6 @@ pub fn day_08() -> Day {
     )
 }
 
-
 fn part1(input: &str) -> String {
     let mut console = Console::new(input);
     console.execute().to_string()
@@ -21,7 +20,6 @@ fn part2(input: &str) -> String {
     console.execute_fixed().to_string()
 }
 
-#[derive(Debug)]
 enum InstructionType {
     Acc,
     Jmp,
@@ -37,7 +35,6 @@ impl InstructionType {
         }
     }
 }
-#[derive(Debug)]
 struct Instruction {
     instruction_type: InstructionType,
     value: i32,
@@ -51,7 +48,7 @@ impl Instruction {
         Self {
             instruction_type,
             value,
-            executions: 0
+            executions: 0,
         }
     }
     fn reset(&mut self) {
@@ -61,7 +58,6 @@ impl Instruction {
 struct Console {
     instructions: Vec<Instruction>,
     ins_ptr: usize,
-    max_ins_ptr: usize,
     accumulator: i32,
 }
 impl Console {
@@ -70,35 +66,58 @@ impl Console {
         Self {
             instructions,
             ins_ptr: 0,
-            max_ins_ptr: 0,
             accumulator: 0,
         }
     }
     fn execute(&mut self) -> i32 {
-        while self.ins_ptr < self.instructions.len() && self.instructions[self.ins_ptr].executions < 1 {
+        while self.ins_ptr < self.instructions.len()
+            && self.instructions[self.ins_ptr].executions < 1
+        {
             self.instructions[self.ins_ptr].executions += 1;
             match self.instructions[self.ins_ptr].instruction_type {
                 InstructionType::Acc => {
                     self.accumulator += self.instructions[self.ins_ptr].value;
                     self.ins_ptr += 1;
-                },
-                InstructionType::Jmp => self.ins_ptr = (self.ins_ptr as i32 + self.instructions[self.ins_ptr].value) as usize,
+                }
+                InstructionType::Jmp => {
+                    self.ins_ptr =
+                        (self.ins_ptr as i32 + self.instructions[self.ins_ptr].value) as usize
+                }
                 InstructionType::Nop => self.ins_ptr += 1,
             }
-            self.max_ins_ptr = self.max_ins_ptr.max(self.ins_ptr);
         }
         self.accumulator
     }
     fn execute_fixed(&mut self) -> i32 {
-        self.execute();
-        self.instructions[self.max_ins_ptr] = Instruction::new("nop 0");
-        self.reset();
-        println!("{:?}", self.instructions);
-        self.execute()
+        for i in 0..self.instructions.len() {
+            match self.instructions[i].instruction_type {
+                InstructionType::Jmp => {
+                    self.instructions[i].instruction_type = InstructionType::Nop
+                }
+                InstructionType::Nop => {
+                    self.instructions[i].instruction_type = InstructionType::Jmp
+                }
+                _ => continue,
+            }
+            self.reset();
+            let result = self.execute();
+            if self.ins_ptr == self.instructions.len() {
+                return result;
+            }
+            match self.instructions[i].instruction_type {
+                InstructionType::Jmp => {
+                    self.instructions[i].instruction_type = InstructionType::Nop
+                }
+                InstructionType::Nop => {
+                    self.instructions[i].instruction_type = InstructionType::Jmp
+                }
+                _ => (),
+            }
+        }
+        panic!("No solution found");
     }
     fn reset(&mut self) {
         self.ins_ptr = 0;
-        self.max_ins_ptr = 0;
         self.accumulator = 0;
         self.instructions.iter_mut().for_each(|f| f.reset());
     }
