0f05e2628ec6dc16c88dde7c479228959c94007b
Antonio Mamić <antoniomamic007@gmail.com>
2024-07-20 16:52:43 +0200

    improve AoC-2016-11

--- Advent of Code/src/challenges/year_2016/day_11/mod.rs
+++ Advent of Code/src/challenges/year_2016/day_11/mod.rs
@@ -1,7 +1,5 @@
-use std::cmp::Ordering;
 use crate::shared::structures::Day;
-use std::collections::{HashMap, VecDeque};
-use std::hash::Hash;
+use std::collections::{HashMap, HashSet, VecDeque};
 use regex::Regex;
 use smallvec::SmallVec;
 
@@ -9,80 +7,114 @@ pub fn day_11() -> Day {
     Day::new(11, include_str!("text.txt"), include_str!("input.txt"), part1, part2)
 }
 
+// the main idea for fast solution is to not differentiate between pairs of elements
+// if element1-microchip is on floor 0, element1-generator on floor 3
+// and element2-microchip is on floor 1, element2-generator on floor 2
+// this state is the same as
+// if element1-microchip is on floor 1, element1-generator on floor 2
+// and element2-microchip is on floor 0, element2-generator on floor 3
+
 fn part1(input: &str) -> String {
     // get initial building state
-    let mut area = parse_input(input);
-    area.floors.iter_mut().for_each(|floor| floor.sort());
+    let state = State::from_input(input);
 
     // setup wanted building state
-    let mut wanted_state = Area::new();
+    let mut wanted_state = State::new();
     wanted_state.elevator = 3;
-    area.floors.iter().for_each(|floor| wanted_state.floors[3].extend(floor.iter().copied()));
-    wanted_state.floors.iter_mut().for_each(|floor| floor.sort());
+    for _ in 0..state.elements.len() {
+        wanted_state.elements.push([3, 3]);
+    }
 
     // search for optimal solution
-    if area != wanted_state {
-        bfs_search(area, wanted_state).to_string()
-    } else {
-        0.to_string()
-    }
+    bfs_search(state, wanted_state).to_string()
 }
 
 fn part2(input: &str) -> String {
     // get initial building state
-    let mut area = parse_input(input);
-    area.floors[0].push(Element { id: u8::MAX, is_generator: false });
-    area.floors[0].push(Element { id: u8::MAX, is_generator: true });
-    area.floors[0].push(Element { id: u8::MAX - 1, is_generator: false });
-    area.floors[0].push(Element { id: u8::MAX - 1, is_generator: true });
-    area.floors.iter_mut().for_each(|floor| floor.sort());
+    let mut state = State::from_input(input);
+    state.elements.push([0, 0]);
+    state.elements.push([0, 0]);
+    state.elements.sort();
 
     // setup wanted building state
-    let mut wanted_state = Area::new();
+    let mut wanted_state = State::new();
     wanted_state.elevator = 3;
-    area.floors.iter().for_each(|floor| wanted_state.floors[3].extend(floor.iter().copied()));
-    wanted_state.floors.iter_mut().for_each(|floor| floor.sort());
+    for _ in 0..state.elements.len() {
+        wanted_state.elements.push([3, 3]);
+    }
 
     // search for optimal solution
-    if area != wanted_state {
-        bfs_search(area, wanted_state).to_string()
-    } else {
-        0.to_string()
-    }
+    bfs_search(state, wanted_state).to_string()
 }
 
 #[derive(Clone, Eq, PartialEq, Hash)]
-struct Area {
+struct State {
     elevator: u8,
-    floors: [SmallVec<[Element; 14]>; 4],
+    elements: SmallVec<[[u8; 2]; 10]>,  // vector of elements, with [microchip, generator] (0..3 -> floor)
 }
-impl Area {
+impl State {
     fn new() -> Self {
-        Area {
+        Self {
             elevator: 0,
-            floors: [
-                SmallVec::with_capacity(14),
-                SmallVec::with_capacity(14),
-                SmallVec::with_capacity(14),
-                SmallVec::with_capacity(14),
-            ],
+            elements: SmallVec::with_capacity(10),
         }
     }
-    
-    fn is_valid(&self) -> bool {
-        if self.floors[self.elevator as usize].is_empty() {
-            return false;
+
+    /// Generate initial building state from input
+    fn from_input(input: &str) -> Self {
+        let mut element_id = HashMap::new();
+        let mut floors = vec![vec![]; 4];
+
+        let re = Regex::new(r", and|and|,").unwrap();
+        for (i, floor_line) in input.lines().enumerate() {
+            if floor_line.contains("nothing relevant") {
+                continue;
+            }
+            re.split(floor_line
+                    .trim_end_matches('.')
+                    .split_once(" contains ")
+                    .unwrap()
+                    .1)
+                .for_each(|element| {
+                    let mut element = element.trim().trim_start_matches("a ");
+                    let is_generator = if element.ends_with("generator") {
+                        element = element.trim_end_matches(" generator");
+                        true
+                    } else {
+                        element = element.trim_end_matches("-compatible microchip");
+                        false
+                    };
+                    let len = element_id.len() as u8;
+                    let id = *element_id.entry(element).or_insert(len);
+                    floors[i].push((id, is_generator));
+                });
         }
-        for floor in &self.floors {
-            // if there are no generators, then floor must be valid
-            if floor.iter().any(|element| element.is_generator) {
-                for element in floor {
-                    if !element.is_generator {
-                        let mut element_generator = *element;
-                        element_generator.is_generator = true;
-                        if !floor.contains(&element_generator) {
-                            return false;
-                        }
+
+        let mut elements = Vec::with_capacity(element_id.len());
+        for _ in 0..element_id.len() {
+            elements.push([0, 0]);
+        }
+        for (i, floor) in floors.into_iter().enumerate() {
+            for (id, is_generator) in floor {
+                elements[id as usize][usize::from(is_generator)] = i as u8;
+            }
+        }
+
+        let mut state = Self::new();
+        state.elements.extend(elements);
+        state.elements.sort();
+
+        state
+    }
+
+    /// Check if the state is valid
+    fn is_valid(&self) -> bool {
+        for floor in 0..4 {
+            // if there is no generator on the floor, it must be valid
+            if self.elements.iter().any(|element| element[1] == floor) {
+                for element in &self.elements {
+                    if element[0] == floor && element[0] != element[1] {
+                        return false;
                     }
                 }
             }
@@ -91,70 +123,20 @@ impl Area {
     }
 }
 
-/// Element representing microchip or generator
-#[derive(Copy, Clone, Eq, PartialEq, Hash)]
-struct Element {
-    id: u8,              // id of the type of element
-    is_generator: bool,  // true if element is a generator, false if it is a microchip
-}
-impl PartialOrd for Element {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-impl Ord for Element {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let mut cmp_result = self.id.cmp(&other.id);
-        if let Ordering::Equal = cmp_result {
-            cmp_result = self.is_generator.cmp(&other.is_generator);
-        }
-        cmp_result
-    }
-}
-
-/// Generate initial building state from input
-fn parse_input(input: &str) -> Area {
-    let mut id_map = HashMap::new();
-    let mut area = Area::new();
-
-    let re = Regex::new(r", and|and|,").unwrap();
-    for (i, floor_line) in input.lines().enumerate() {
-        if floor_line.contains("nothing relevant") {
-            continue;
-        }
-        re.split(floor_line
-            .trim_end_matches('.')
-            .split_once(" contains ")
-            .unwrap()
-            .1)
-            .for_each(|element| {
-                let mut element = element.trim().trim_start_matches("a ");
-                let mut is_generator = false;
-                if element.ends_with("generator") {
-                    is_generator = true;
-                    element = element.trim_end_matches(" generator");
-                } else {
-                    element = element.trim_end_matches("-compatible microchip");
-                }
-                let len = id_map.len() as u8;
-                let id = *id_map.entry(element).or_insert(len);
-                area.floors[i].push(Element { id, is_generator });
-            });
+/// Search for the minimum number of steps to get from initial_state to wanted_state using BFS.
+/// Panics if no solution is found (there is no way to get to wanted_state from initial_state).
+fn bfs_search(initial_state: State, wanted_state: State) -> i32 {
+    if initial_state == wanted_state {
+        return 0;
     }
 
-    area
-}
-
-/// Search for the minimum number of steps to get from initial_state to wanted_state using BFS algorithm
-/// Panics if no solution is found, that is there isn't any way to achieve wanted_state starting from initial_state.
-fn bfs_search(initial_state: Area, wanted_state: Area) -> i32 {
-    let mut state_step_map = HashMap::new();
+    let mut seen_states = HashSet::new();
     let mut queue = VecDeque::new();
-    state_step_map.insert(initial_state.clone(), 0);
-    queue.push_back(initial_state);
+    seen_states.insert(initial_state.clone());
+    queue.push_back((initial_state, 0));
 
     let mut up_down_moves = Vec::with_capacity(2);
-    while let Some(state) = queue.pop_front() {
+    while let Some((state, steps)) = queue.pop_front() {
         up_down_moves.clear();
         if state.elevator > 0 {
             up_down_moves.push(-1);
@@ -164,39 +146,41 @@ fn bfs_search(initial_state: Area, wanted_state: Area) -> i32 {
         }
 
         for &k in &up_down_moves {
-            for i in 0..state.floors[state.elevator as usize].len() {
-                // moving 1 element
-                let mut next_state = state.clone();
-                let removed = next_state.floors[next_state.elevator as usize].remove(i);
-                next_state.elevator = next_state.elevator.wrapping_add_signed(k);
-                next_state.floors[next_state.elevator as usize].push(removed);
-                next_state.floors[next_state.elevator as usize].sort();
-                
-                if !state_step_map.contains_key(&next_state) && next_state.is_valid() {
-                    state_step_map.insert(next_state.clone(), state_step_map.get(&state).unwrap() + 1);
-                    if next_state == wanted_state {
-                        return *state_step_map.get(&next_state).unwrap();
-                    } else {
-                        queue.push_back(next_state);
+            // moving 1 element
+            for i in 0..(state.elements.len() << 1) {
+                if state.elements[i >> 1][i & 1] == state.elevator {
+                    let mut next_state = state.clone();
+                    next_state.elevator = next_state.elevator.wrapping_add_signed(k);
+                    next_state.elements[i >> 1][i & 1] = next_state.elements[i >> 1][i & 1].wrapping_add_signed(k);
+                    next_state.elements.sort();
+                    if next_state.is_valid() && !seen_states.contains(&next_state) {
+                        if next_state == wanted_state {
+                            return steps + 1;
+                        } else {
+                            queue.push_back((next_state.clone(), steps + 1));
+                            seen_states.insert(next_state);
+                        }
                     }
                 }
+            }
 
-                // moving 2 elements
-                for j in (i + 1)..state.floors[state.elevator as usize].len() {
-                    let mut next_state = state.clone();
-                    let removed1 = next_state.floors[next_state.elevator as usize].remove(j);
-                    let removed2 = next_state.floors[next_state.elevator as usize].remove(i);
-                    next_state.elevator = next_state.elevator.wrapping_add_signed(k);
-                    next_state.floors[next_state.elevator as usize].push(removed1);
-                    next_state.floors[next_state.elevator as usize].push(removed2);
-                    next_state.floors[next_state.elevator as usize].sort();
-                    
-                    if !state_step_map.contains_key(&next_state) && next_state.is_valid() {
-                        state_step_map.insert(next_state.clone(), state_step_map.get(&state).unwrap() + 1);
-                        if next_state == wanted_state {
-                            return *state_step_map.get(&next_state).unwrap();
-                        } else {
-                            queue.push_back(next_state);
+            for i in 0..(state.elements.len() << 1) {
+                if state.elements[i >> 1][i & 1] == state.elevator {
+                    for j in (i + 1)..(state.elements.len() << 1) {
+                        if state.elements[j >> 1][j & 1] == state.elevator {
+                            let mut next_state = state.clone();
+                            next_state.elevator = next_state.elevator.wrapping_add_signed(k);
+                            next_state.elements[i >> 1][i & 1] = next_state.elements[i >> 1][i & 1].wrapping_add_signed(k);
+                            next_state.elements[j >> 1][j & 1] = next_state.elements[j >> 1][j & 1].wrapping_add_signed(k);
+                            next_state.elements.sort();
+                            if next_state.is_valid() && !seen_states.contains(&next_state) {
+                                if next_state == wanted_state {
+                                    return steps + 1;
+                                } else {
+                                    queue.push_back((next_state.clone(), steps + 1));
+                                    seen_states.insert(next_state);
+                                }
+                            }
                         }
                     }
                 }
