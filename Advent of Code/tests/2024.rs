3d177aec378fb5822ac36f94c49b6fc608d92c39
Antonio Mamić <antoniomamic007@gmail.com>
2024-12-02 20:34:45 +0100

    add AoC 2024 - Day 02

--- Advent of Code/tests/2024.rs
+++ Advent of Code/tests/2024.rs
@@ -10,3 +10,13 @@ fn year2024_day01_part1() {
 fn year2024_day01_part2() {
     run_test(2024, 1, 2, "21024792");
 }
+
+#[test]
+fn year2024_day02_part1() {
+    run_test(2024, 2, 1, "282");
+}
+
+#[test]
+fn year2024_day02_part2() {
+    run_test(2024, 2, 2, "349");
+}
