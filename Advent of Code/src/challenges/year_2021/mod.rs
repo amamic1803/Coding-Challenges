d231d1cb0dde6e4443ce907a290ed887bd12f08e
DarkLord76865 <mmima191@gmail.com>
2024-12-07 14:20:40 +0100

    add AoC 2021 - Day 10

--- Advent of Code/src/challenges/year_2021/mod.rs
+++ Advent of Code/src/challenges/year_2021/mod.rs
@@ -7,6 +7,7 @@ pub mod day_04;
 pub mod day_05;
 pub mod day_06;
 pub mod day_09;
+pub mod day_10;
 
 use day_01::day_01;
 use day_02::day_02;
@@ -15,7 +16,8 @@ use day_04::day_04;
 use day_05::day_05;
 use day_06::day_06;
 use day_09::day_09;
+use day_10::day_10;
 
 pub fn year_2021() -> Year {
-    Year::new(2021, vec![day_01(), day_02(), day_03(), day_04(), day_05(), day_06(), day_09()])
+    Year::new(2021, vec![day_01(), day_02(), day_03(), day_04(), day_05(), day_06(), day_09(), day_10()])
 }
