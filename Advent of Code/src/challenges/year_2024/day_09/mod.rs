c9731f732f73d604e5d3cf5826e64361720feccb
Antonio Mamić <antoniomamic007@gmail.com>
2025-03-04 23:27:00 +0100

    work on AoC 2024 - Day 9 - Part 2

--- Advent of Code/src/challenges/year_2024/day_09/mod.rs
+++ Advent of Code/src/challenges/year_2024/day_09/mod.rs
@@ -10,7 +10,7 @@ fn part1(input: &str) -> String {
 }
 
 fn part2(input: &str) -> String {
-    compress2(input).enumerate().map(|(i, block)| i as u64 * block as u64).sum::<u64>().to_string()
+    compress2(input).enumerate().map(|(i, block)| i as u64 * block.unwrap_or(0) as u64).sum::<u64>().to_string()
 }
 
 fn compress1(disc: &str) -> impl Iterator<Item = u32> {
@@ -19,39 +19,106 @@ fn compress1(disc: &str) -> impl Iterator<Item = u32> {
         disc.pop();
     }
     let mut i = 0;
-    iter::from_fn(move || loop {
-        if i >= disc.len() {
-            return None;
-        }
-
-        if disc[i] != 0 {
-            disc[i] -= 1;
-            return Some(i as u32 >> 1);
-        }
-
-        let last_ind = disc.len() - 1;
-        if disc[last_ind] != 0 {
-            if disc[i + 1] != 0 {
-                disc[i + 1] -= 1;
-                disc[last_ind] -= 1;
-                return Some(last_ind as u32 >> 1);
-            } else {
-                i += 2;
+    iter::from_fn(move || {
+        loop {
+            if i >= disc.len() {
+                return None;
             }
-        } else {
-            disc.pop();
-            disc.pop();
+
+            if disc[i] != 0 {
+                disc[i] -= 1;
+                return Some(i as u32 >> 1);
+            }
+
+            let last_ind = disc.len() - 1;
+            if disc[last_ind] != 0 {
+                if disc[i + 1] != 0 {
+                    disc[i + 1] -= 1;
+                    disc[last_ind] -= 1;
+                    return Some(last_ind as u32 >> 1);
+                } else {
+                    i += 2;
+                }
+            } else {
+                disc.pop();
+                disc.pop();
+            }
         }
     })
 }
 
-fn compress2(disc: &str) -> impl Iterator<Item = u32> {
-    let mut compacted_disc = Vec::with_capacity(20000 * 9);
-    for (i, c) in disc.trim().chars().enumerate() {
-        let amount = c.to_digit(10).unwrap() as u8;
-        let data = if i % 2 == 0 { Some(i) } else { None };
-        compacted_disc.extend(iter::repeat(data).take(amount as usize));
+fn compress2(disc: &str) -> impl Iterator<Item = Option<u32>> + use<> {
+    let mut disc = disc.trim().chars().map(|c| c.to_digit(10).unwrap() as u8).collect::<Vec<_>>();
+    if disc.len() % 2 == 0 {
+        disc.pop();
+    }
+    let mut files = Vec::with_capacity(disc.len() / 2 + 1);
+    let mut free_space = Vec::with_capacity(disc.len() / 2);
+    let mut position = 0;
+    let mut file_index = 0;
+    for (i, item) in disc.into_iter().enumerate() {
+        if i % 2 == 0 {
+            // file
+            // (disc_position_index, file_order_index, file_size)
+            files.push((position, file_index, item));
+            file_index += 1;
+        } else {
+            // free space
+            // (disc_position_index, free_space_size)
+            free_space.push((position, item));
+        }
+        position += item as u32;
     }
 
-    compacted_disc.into_iter().map(|data| data.unwrap_or(0) as u32)
+    for file in files.iter_mut().rev() {
+        for free_block in free_space.iter_mut() {
+            if file.2 <= free_block.1 {
+                // file fits in a free block
+                file.0 = free_block.0;
+                free_block.0 += file.2 as u32;
+                free_block.1 -= file.2;
+                break;
+            } else if free_block.0 > file.0 {
+                // no free block to the left fits the file
+                break;
+            }
+        }
+    }
+
+    files.sort_by_key(|file| file.0);
+    let mut i = 0; // disc position
+    let mut j = 0; // index of the file based on the new order
+    // temp variable to count the number of times the file id is repeated
+    // (file size)
+    let mut k = 0;
+    iter::from_fn(move || {
+        loop {
+            // if there are no more files, end the iterator
+            if j >= files.len() {
+                return None;
+            }
+
+            // if a disc position is lower than the next file we are looking at, return None (means '.')
+            if i < files[j].0 {
+                i += 1;
+                return Some(None);
+            } else if i == files[j].0 {
+                // if a disc position is equal to the position of the next file,
+                // it means it has just been reached, and counter k needs to be set up
+                k = files[j].2;
+                i += 1;
+                continue;
+            } else {
+                // here we use k to determine when the EOF is reached
+                if k > 0 {
+                    k -= 1;
+                    return Some(Some(files[j].1));
+                } else {
+                    i = files[j].0 + files[j].2 as u32;
+                    j += 1;
+                    continue;
+                }
+            }
+        }
+    })
 }
